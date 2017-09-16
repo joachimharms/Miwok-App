7e1f17b72ce9b4e53d61331996087f97c56ec65e
joachimharms <joachimharms77@gmail.com>
2017-09-16 20:30:48 +0200

    Add stringArray with english numbers - done

--- app/src/main/java/com/example/android/miwok/NumberActivity.java
+++ app/src/main/java/com/example/android/miwok/NumberActivity.java
@@ -2,6 +2,7 @@ package com.example.android.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import static android.R.attr.y;
 import static com.example.android.miwok.R.id.numbers;
@@ -24,5 +25,16 @@ public class NumberActivity extends AppCompatActivity {
         numbersArray[7] = "eight";
         numbersArray[8] = "nine";
         numbersArray[9] = "ten";
+
+        Log.v("NumberActivity", "Content of numbersArray[0]: " + numbersArray[0]);
+        Log.v("NumberActivity", "Content of numbersArray[1]: " + numbersArray[1]);
+        Log.v("NumberActivity", "Content of numbersArray[2]: " + numbersArray[2]);
+        Log.v("NumberActivity", "Content of numbersArray[3]: " + numbersArray[3]);
+        Log.v("NumberActivity", "Content of numbersArray[4]: " + numbersArray[4]);
+        Log.v("NumberActivity", "Content of numbersArray[5]: " + numbersArray[5]);
+        Log.v("NumberActivity", "Content of numbersArray[6]: " + numbersArray[6]);
+        Log.v("NumberActivity", "Content of numbersArray[7]: " + numbersArray[7]);
+        Log.v("NumberActivity", "Content of numbersArray[8]: " + numbersArray[8]);
+        Log.v("NumberActivity", "Content of numbersArray[9]: " + numbersArray[9]);
     }
 }
