8edb3bba08c9c36fca0aef31a21d6522b45ee567
joachimharms <joachimharms77@gmail.com>
2017-09-16 19:24:10 +0200

    Add Arrays of english numbers string - done

--- app/src/main/java/com/example/android/miwok/NumberActivity.java
+++ app/src/main/java/com/example/android/miwok/NumberActivity.java
@@ -3,11 +3,26 @@ package com.example.android.miwok;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import static android.R.attr.y;
+import static com.example.android.miwok.R.id.numbers;
+
 public class NumberActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_number);
+
+    String[] numbersArray = new String[10];
+        numbersArray[0] = "one";
+        numbersArray[1] = "two";
+        numbersArray[2] = "three";
+        numbersArray[3] = "four";
+        numbersArray[4] = "five";
+        numbersArray[5] = "six";
+        numbersArray[6] = "seven";
+        numbersArray[7] = "eight";
+        numbersArray[8] = "nine";
+        numbersArray[9] = "ten";
     }
 }
